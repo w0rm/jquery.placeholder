d5be369739fa79557132a7151e690a979acfa8b1
Andrey Kuzmin <unsoundscapes@gmail.com>
2013-08-25 11:44:32 +0400

    Refactor tests

--- spec/test.js
+++ spec/test.js
@@ -9,74 +9,66 @@ define(function (require) {
 
   describe('jQuery placeholder plugin', function () {
 
-    var $textInputWithoutValue
-      , $textInputWithValue
-      , $passwordInputWithoutValue
-      , $passwordInputWithValue
+    var currentId = 0
 
-    beforeEach(function () {
-      $textInputWithoutValue = $('<input id="text1" placeholder="test placeholder" ' +
-                                 'type="text">')
-      $textInputWithValue = $('<input id="text2" value="test value" ' +
-                              'placeholder="test placeholder" type="text">')
-      $passwordInputWithoutValue = $('<input id="password1" placeholder="test placeholder" ' +
-                                     'type="password">')
-      $passwordInputWithValue = $('<input id="password2" value="test value" ' +
-                                  'placeholder="test placeholder" type="password">')
-
-      $('#test').append(
-        $textInputWithoutValue
-      , $textInputWithValue
-      , $passwordInputWithoutValue
-      , $passwordInputWithValue
-      )
-    })
+    function makeInput (newAttrs) {
+      var attrs = { id: 'input' + (++currentId)
+                  , type: 'text'
+                  , value: 'test value'
+                  , placeholder: 'test placeholder'
+                  }
+      return $('<input>')
+               .attr($.extend(attrs, newAttrs))
+               .appendTo('#test')
+    }
 
     it('should register as jquery plugin', function () {
+      var $el = makeInput({value: ''})
       expect($.fn.placeholder).to.exist
-      expect($textInputWithoutValue.placeholder(options))
-        .to.equal($textInputWithoutValue)
+      expect($el.placeholder(options)).to.equal($el)
     })
 
     it('should store itself in data', function () {
-      var placeholderData = $textInputWithoutValue.placeholder(options).data('placeholder')
+      var $el = makeInput({value: ''})
+        , placeholderData = $el.placeholder(options).data('placeholder')
       expect(placeholderData).to.be.an.instanceof(Placeholder)
     })
 
     it('should set empty text input value to placeholder', function () {
-      expect($textInputWithoutValue.attr('placeholder'))
-        .to.equal($textInputWithoutValue.placeholder(options)[0].value)
-      expect($textInputWithoutValue.hasClass(options.className)).to.be.true
+      var $el = makeInput({value: ''})
+      expect($el.attr('placeholder')).to.equal($el.placeholder(options)[0].value)
+      expect($el.hasClass(options.className)).to.be.true
     })
 
     it('should set input value to placeholder if value is set to empty', function () {
-      var placeholderAttr = $passwordInputWithoutValue.attr('placeholder')
-      $textInputWithValue.placeholder(options)
-      $textInputWithValue.val('')
-      expect($textInputWithValue[0].value).to.equal(placeholderAttr)
-      expect($textInputWithValue.hasClass(options.className)).to.be.true
+      var $el = makeInput()
+        , placeholderAttr = $el.attr('placeholder')
+      $el.placeholder(options).val('')
+      expect($el[0].value).to.equal(placeholderAttr)
+      expect($el.hasClass(options.className)).to.be.true
     })
 
     it('should return empty value if placeholder is active', function () {
-      expect($textInputWithoutValue.placeholder(options).val()).to.equal('')
-      expect($passwordInputWithoutValue.placeholder(options).val()).to.equal('')
+      expect(makeInput({value: ''}).placeholder(options).val()).to.equal('')
+      expect(makeInput({type: 'password', value: ''}).placeholder(options).val()).to.equal('')
     })
 
     it('should replace empty password input ' +
        'with text input which value is set to placeholder', function () {
-      var placeholderAttr = $passwordInputWithoutValue.attr('placeholder')
-        , idAttr = $passwordInputWithoutValue.attr('id')
-      $passwordInputWithoutValue.placeholder(options)
+      var $el = makeInput({type: 'password', value: ''})
+        , placeholderAttr = $el.attr('placeholder')
+        , idAttr = $el.attr('id')
+      $el.placeholder(options)
       expect($('#' + idAttr).attr('type')).to.equal('text')
       expect($('#' + idAttr)[0].value).to.equal(placeholderAttr)
       expect($('#' + idAttr).hasClass(options.className)).to.be.true
     })
 
     it('should keep input value', function () {
-      expect($textInputWithValue[0].value)
-        .to.equal($textInputWithValue.placeholder(options)[0].value)
-      expect($passwordInputWithValue[0].value)
-        .to.equal($passwordInputWithValue.placeholder(options)[0].value)
+      var $txtEl = makeInput()
+        , $passEl = makeInput({type: 'password'})
+      expect($txtEl[0].value).to.equal($txtEl.placeholder(options)[0].value)
+      expect($passEl[0].value).to.equal($passEl.placeholder(options)[0].value)
     })
 
     afterEach(function () {
